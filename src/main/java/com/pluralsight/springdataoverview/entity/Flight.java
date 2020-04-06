d5cf38c717ab1d964dd0eb0b3b2ea0078bf7d206
ernestkoko <ernesteferetin@yahoo.com>
2020-04-06 23:14:02 +0100

    Initial commit

---
+++ src/main/java/com/pluralsight/springdataoverview/entity/Flight.java
@@ -0,0 +1,11 @@
+package com.pluralsight.springdataoverview.entity;
+
+import java.time.LocalDateTime;
+
+public class Flight {
+    private Long id;
+    private String origin;
+    private String destination;
+    private LocalDateTime scheduledAt;
+
+}
