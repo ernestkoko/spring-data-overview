c9009229fd90309c4a7805548d544925d1abcaf0
ernestkoko <ernesteferetin@yahoo.com>
2020-04-08 13:34:00 +0100

    Created an interface class, FlightRepository and went to test class to wright test to check if the instantiated class was saved

--- src/test/java/com/pluralsight/springdataoverview/SpringDataOverviewApplicationTests.java
+++ src/test/java/com/pluralsight/springdataoverview/SpringDataOverviewApplicationTests.java
@@ -1,6 +1,7 @@
 package com.pluralsight.springdataoverview;
 
 import com.pluralsight.springdataoverview.entity.Flight;
+import com.pluralsight.springdataoverview.repository.FlightRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,31 +17,56 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-class SpringDataOverviewApplicationTests {
+class CrudTests {
 
 	@Autowired
-	private EntityManager entityManager;
+	private FlightRepository flightRepository;
+
+
+
 
 	@Test
-	public void verifyFlightCanBeSaved() {
-		final Flight flight = new Flight();
-		flight.setOrigin("Amsterdam");
+	public void shoudlPerformCRUDOperations(){
+		Flight flight = new Flight();
+		flight.setOrigin("London");
 		flight.setDestination("New York");
 		flight.setScheduledAt(LocalDateTime.parse("2011-12-13T12:12:00"));
 
-		entityManager.persist(flight);  // this stores it in h2 db
-     //creating a query
-		final TypedQuery<Flight> results = entityManager
-				.createQuery("SELECT f FROM Flight f", Flight.class);
+		flightRepository.save(flight);
 
-		final List<Flight> resultList = results.getResultList();
+		//checking if the flight is saved
+		Assertions.assertThat(flightRepository.findAll())  //loads all the flights
+				.hasSize(1)   //checks if the size is 1
+				.first()       //picks the first flight
+				.isEqualToComparingFieldByField(flight);   //and compare the flight to the flight we have here
 
-		//comparing the two flights
-		Assertions.assertThat(resultList)
-				.hasSize(1)
-				.first()
-				.isEqualTo(flight);
+		flightRepository.deleteById(flight.getId());  //this deletes the flight
 
 	}
+
+//	@Autowired
+//	private EntityManager entityManager;
+//
+//	@Test
+//	public void verifyFlightCanBeSaved() {
+//		final Flight flight = new Flight();
+//		flight.setOrigin("Amsterdam");
+//		flight.setDestination("New York");
+//		flight.setScheduledAt(LocalDateTime.parse("2011-12-13T12:12:00"));
+//
+//		entityManager.persist(flight);  // this stores it in h2 db
+//     //creating a query
+//		final TypedQuery<Flight> results = entityManager
+//				.createQuery("SELECT f FROM Flight f", Flight.class);
+//
+//		final List<Flight> resultList = results.getResultList();
+//
+//		//comparing the two flights
+//		Assertions.assertThat(resultList)
+//				.hasSize(1)
+//				.first()
+//				.isEqualTo(flight);
+//
+//	}
 
 }
